a4a3328c687bdc82cf5f6874325e0079eb3e2082
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-07-09 12:06:06 +0200

    Initial Commit

---
+++ SearchBox/SearchBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBoxApp.swift
+//  SearchBox
+//
+//  Created by softphone on 09/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SearchBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
